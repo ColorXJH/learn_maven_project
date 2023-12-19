06a849693be17de4985a5d127a6aae4129fcbba7
ColorXJH <1204332316@qq.com>
2023-12-19 16:08:58 +0800

    Initial commit

---
+++ pro03-module-java/src/test/java/org/example/MainTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.Test;
+
+/**
+ * @author ColorXJH
+ * @version 1.0
+ * @description:
+ * @date 2023-12-19 14:44
+ */
+public class MainTest {
+    @Test
+    public void helloTest(){
+        Main main=new Main();
+        main.hello();
+    }
+}
---
+++ pro04-module-web/src/main/java/org/example/Message.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author ColorXJH
+ * @version 1.0
+ * @description:
+ * @date 2023-12-19 15:55
+ */
+public class Message {
+    public String getMessage(){
+        return "hello Message";
+    }
+}
